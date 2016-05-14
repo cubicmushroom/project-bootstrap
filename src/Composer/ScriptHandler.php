c6a62c4a69ce9abdc00ec3cd2ebe537a3b450480
Toby Griffiths <toby@cubicmushroom.co.uk>
2016-05-14 17:48:25 +0100

    Added script to initialise project

---
+++ src/Composer/ScriptHandler.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Author: Toby Griffiths <toby@cubicmushroom.co.uk>
+ * Date: 14/05/2016
+ */
+
+namespace CubicMushroom\ProjectBootstrap\Composer;
+
+use Composer\Script\Event;
+
+/**
+ * Composer scripts to run during composer hooks
+ *
+ * @package CubicMushroom\ProjectBootstrap
+ */
+class ScriptHandler
+{
+    /**
+     * Prompts the user for project details
+     *
+     * @param Event $event
+     */
+    public static function initialiseProject(Event $event)
+    {
+        echo 'Hello world!';
+    }
+}
